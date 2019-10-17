6711d8c9855a5a3117a58dc65dff6a5c1e4c1a9e
Matt Durrant <matt@mattdurrant.com>
2019-10-17 21:52:05 +0100

    Installs as a windows service

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const spotify   = require('./spotify.js')
 const output    = require('./output.js')
 const uploader  = require('./uploader.js')
 const schedule  = require('node-schedule')
- 
-; (async () => {
-  
+const Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'SpotifyFavouriteAlbums',
+  description: 'Searches spotify for my 100 favourite albums and then uploads the results to my website.',
+  script: 'C:\\Code\\spotify-favourite-albums\\index.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  start()
+});
+
+svc.install();
+
+
+async function start() {
   console.log('Lets get started')
   getFavouriteAlbums()
   schedule.scheduleJob('0 * * * *', function(){
     console.log(`Finding vinyl at ${new Date().toISOString()}`)
     getFavouriteAlbums()
   });
-})()
+}
 
 async function getFavouriteAlbums() {
   let albums = await spotify.getAlbums()
