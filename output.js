51f82a6fe465e62eef411f15b4bfd8eb96ae4420
Matt Durrant <matt@mattdurrant.com>
2020-04-12 15:01:32 +0100

    Crown symbol for loved tracks.

--- output.js
+++ output.js
@@ -21,6 +21,8 @@ function buildHtml(albums) {
     header += `<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>`
     header += `<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>`
     header += `<link rel='stylesheet' href='https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' type='text/css' media='all' />`
+    header += `<link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet">`
+
     header += `<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>`
     // header += `<script>$(document).ready( function () { $('#results').DataTable({ paging: false, "columns": [{ "orderable": false }, null, null ] }); } );</script>`
 
@@ -37,7 +39,7 @@ function buildHtml(albums) {
         for (let trackStatus of albums[i].tracksStatus)
         {
             if (trackStatus.score == 1) {
-                body += `<a href="${trackStatus.href}">${trackStatus.track}. ${trackStatus.name} 👑</br>`
+                body += `<a href="${trackStatus.href}">${trackStatus.track}. ${trackStatus.name}<i class="em em-trophy" aria-role="presentation" aria-label="TROPHY"></i></br>`
             } 
             else if (trackStatus.score > 0) {
                 body += `<a href="${trackStatus.href}">${trackStatus.track}. ${trackStatus.name}</br>`
